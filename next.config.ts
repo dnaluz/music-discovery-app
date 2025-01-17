f9b7be6462a468c2d12c48d2befabb1436b0f35b
Dave Naluz <dave.naluz@versett.com>
2025-01-17 18:16:00 -0500

    Updated next configuration to allow images from hostname

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.discogs.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'st.discogs.com'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
