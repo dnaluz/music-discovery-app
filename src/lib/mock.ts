85c49fac14e30d27d8a17cfa82c27f60505c9f14
Dave Naluz <dave.naluz@versett.com>
2025-01-17 18:28:02 -0500

    Mock data for test suite

---
+++ src/lib/mock.ts
@@ -0,0 +1,53 @@
+const albums: {albumId: number, title: string, year: string, coverImage:string}[] = [
+  {
+      "albumId": 30755466,
+      "title": "Theremin Man - Songs For The Future",
+      "year": "2024",
+      "coverImage": "https://i.discogs.com/gE61nlTlCpyD4giLnhQNaB59e82RKGSp6oVHFoXYSbI/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTMwNzU1/NDY2LTE3MTY0MjY5/NjgtODM3NS5qcGVn.jpeg"
+  },
+  {
+      "albumId": 30432245,
+      "title": "Scenic Route To Alaska - Lasts Forever",
+      "year": "2024",
+      "coverImage": "https://i.discogs.com/ZH26weYqBOmHOzMM1Ys-QAExwoWKica1IU3Rmcx8T1w/rs:fit/g:sm/q:90/h:619/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTMwNDMy/MjQ1LTE3MTM0NDYy/NjktNjk4Ni5qcGVn.jpeg"
+  },
+  {
+      "albumId": 31079153,
+      "title": "Dan-E-O - Vigilante",
+      "year": "2024",
+      "coverImage": "https://i.discogs.com/TCuIPEHEKnVg7oBVjUk56cGUJeAMd1QRQry3FBRYWAA/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTMxMDc5/MTUzLTE3MTk1NjU1/NTUtOTU0MC5qcGVn.jpeg"
+  },
+  {
+      "albumId": 32023268,
+      "title": "The Band Whose Name Is A Symbol - Ottawa Psychfest 2024",
+      "year": "2024",
+      "coverImage": "https://i.discogs.com/b-6AVt8HkUo2AzH0LMFBnSmTsA_HOpGnTb0Cyu_thlY/rs:fit/g:sm/q:90/h:848/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTMyMDIz/MjY4LTE3MjkxMjYy/MzktNzY4NS5qcGVn.jpeg"
+  },
+  {
+      "albumId": 30831351,
+      "title": "Various - First Person Shooter (The Definitive FPS Documentary)",
+      "year": "2024",
+      "coverImage": "https://i.discogs.com/-Og5zSCau_VRHyJhgePTw_BHu3muez4YwOJ-nh7BPvc/rs:fit/g:sm/q:90/h:399/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTMwODMx/MzUxLTE3MTcxNTE3/MjAtNjM0OC5qcGVn.jpeg"
+  },
+  {
+      "albumId": 29406169,
+      "title": "Friesen*, Hume*, Waters* - Nightspeak",
+      "year": "2024",
+      "coverImage": "https://i.discogs.com/oGVdROKKktWTBdkt4sUT-ymSNQHNL0K1cg-1_qjaH1U/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTI5NDA2/MTY5LTE3MDQ1MTA3/NzUtOTA2Ny5qcGVn.jpeg"
+  },
+  {
+      "albumId": 31344145,
+      "title": "Lina Allemano's Ohrenschmaus With Dan Peter Sundland, Michael Griener And Special Guest Andrea Parkins - Flip Side",
+      "year": "2024",
+      "coverImage": "https://i.discogs.com/uLd0hb0sgrPHtY7GdA9JEucZBaYJxRSQDCYTyMTRrZ8/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTMxMzQ0/MTQ1LTE3MjIyMDg3/MjMtOTAyMS5qcGVn.jpeg"
+  },
+  {
+      "albumId": 31588390,
+      "title": "Michael Cloud Duguay* - Succeeder",
+      "year": "2024",
+      "coverImage": "https://i.discogs.com/agyAF2tMQS3bNe7nenK3fefc1xfDTdme2cxHQFbC8os/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTMxNTg4/MzkwLTE3MjQ3NzQ0/MzctNzQ4OS5qcGVn.jpeg"
+  }
+]
+
+
+export { albums };
